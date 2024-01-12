0f118271a6ea253cbcd6b30cea88ab87f42da7dd
Quinten Roets <quinten.roets@gmail.com>
2024-01-11 22:41:48 -0800

    adapt tests to new package-test-utils

--- tests/cli/test_create_entry_point.py
+++ tests/cli/test_create_entry_point.py
@@ -5,7 +5,7 @@ from collections.abc import Callable
 import pytest
 import test_instantiate_from_cli_args
 from _pytest.monkeypatch import MonkeyPatch
-from package_dev_tools.utils.tests import clear_cli_args, set_cli_args
+from package_dev_utils.tests.args import cli_args, no_cli_args
 from package_utils.cli import create_entry_point
 from pytest import CaptureFixture
 
@@ -39,44 +39,38 @@ def methods() -> tuple[Callable[..., str | None], ...]:
     return run_with_arguments, run, Options.run
 
 
-def test_entry_point(
-    monkeypatch: MonkeyPatch, methods: tuple[Callable[..., str | None], ...]
-) -> None:
-    clear_cli_args(monkeypatch)
+@no_cli_args
+def test_entry_point(methods: tuple[Callable[..., str | None], ...]) -> None:
     for method in methods:
         entry_point = create_entry_point(method)
         entry_point()
 
 
-def test_with_class_specified(
-    monkeypatch: MonkeyPatch, methods: tuple[Callable[..., str | None], ...]
-) -> None:
-    clear_cli_args(monkeypatch)
+@no_cli_args
+def test_with_class_specified(methods: tuple[Callable[..., str | None], ...]) -> None:
     entry_point = create_entry_point(Options.run, Options)
     entry_point()
 
 
-def test_option(
-    monkeypatch: MonkeyPatch, methods: tuple[Callable[..., str | None], ...]
-) -> None:
-    clear_cli_args(monkeypatch)
-    for method in methods:
-        entry_point = create_entry_point(method)
-        result = entry_point()
-        assert result is None
-    set_cli_args(monkeypatch, "--debug")
-    for method in methods:
-        entry_point = create_entry_point(method)
-        result = entry_point()
-        assert result == Options.message
+def test_option(methods: tuple[Callable[..., str | None], ...]) -> None:
+    with no_cli_args:
+        for method in methods:
+            entry_point = create_entry_point(method)
+            result = entry_point()
+            assert result is None
+    with cli_args("--debug"):
+        for method in methods:
+            entry_point = create_entry_point(method)
+            result = entry_point()
+            assert result == Options.message
 
 
+@cli_args("--help")
 def test_docstring(
     monkeypatch: MonkeyPatch,
     methods: tuple[Callable[..., str | None], ...],
     capsys: CaptureFixture[str],
 ) -> None:
-    set_cli_args(monkeypatch, "--help")
     for method in methods:
         entry_point = create_entry_point(method)
         with pytest.raises(SystemExit) as exception:
--- tests/cli/test_instantiate_from_cli_args.py
+++ tests/cli/test_instantiate_from_cli_args.py
@@ -5,7 +5,7 @@ from dataclasses import dataclass, field
 import click
 import pytest
 from _pytest.monkeypatch import MonkeyPatch
-from package_dev_tools.utils.tests import clear_cli_args, set_cli_args
+from package_dev_utils.tests.args import cli_args, no_cli_args
 from package_utils.cli import instantiate_from_cli_args
 from plib import Path
 from pytest import CaptureFixture
@@ -27,55 +27,55 @@ class Options:
         self.verbosity = 0
 
 
-def test_defaults(monkeypatch: MonkeyPatch) -> None:
-    clear_cli_args(monkeypatch)
+@no_cli_args
+def test_defaults() -> None:
     options = instantiate_from_cli_args(Options)
     assert options.debug == Options.debug
     assert options.config_path == Options.config_path
     assert options.log_path == Options.log_path
 
 
-def test_debug_attribute_true(monkeypatch: MonkeyPatch) -> None:
-    set_cli_args(monkeypatch, "--debug")
+@cli_args("--debug")
+def test_debug_attribute_true() -> None:
     options = instantiate_from_cli_args(Options)
     assert options.debug is True
 
 
-def test_debug_attribute_false(monkeypatch: MonkeyPatch) -> None:
-    set_cli_args(monkeypatch, "--no-debug")
+@cli_args("--no-debug")
+def test_debug_attribute_false() -> None:
     options = instantiate_from_cli_args(Options)
     assert options.debug is False
 
 
-def test_config_path(monkeypatch: MonkeyPatch) -> None:
+def test_config_path() -> None:
     with Path.tempfile() as tmp:
-        set_cli_args(monkeypatch, "--config-path", tmp)
-        options = instantiate_from_cli_args(Options)
-        assert options.config_path == tmp
+        with cli_args("--config-path", tmp):
+            options = instantiate_from_cli_args(Options)
+            assert options.config_path == tmp
 
 
-def test_log_path(monkeypatch: MonkeyPatch) -> None:
+def test_log_path() -> None:
     with Path.tempfile() as tmp:
-        set_cli_args(monkeypatch, "--log-path", tmp)
-        options = instantiate_from_cli_args(Options)
-        assert options.log_path == tmp
+        with cli_args("--log-path", tmp):
+            options = instantiate_from_cli_args(Options)
+            assert options.log_path == tmp
 
 
-def test_message(monkeypatch: MonkeyPatch) -> None:
+def test_message() -> None:
     message = "Hello!"
-    set_cli_args(monkeypatch, "--message", message)
-    options = instantiate_from_cli_args(Options)
-    assert options.message == message
+    with cli_args("--message", message):
+        options = instantiate_from_cli_args(Options)
+        assert options.message == message
 
 
+@cli_args("--verbosity", 1)
 def test_verbosity_attribute_not_exposed(monkeypatch: MonkeyPatch) -> None:
-    set_cli_args(monkeypatch, "--verbosity", 1)
     with pytest.raises(click.exceptions.NoSuchOption):
         instantiate_from_cli_args(Options)
 
 
-def test_help(monkeypatch: MonkeyPatch, capsys: CaptureFixture[str]) -> None:
-    set_cli_args(monkeypatch, "--help")
+@cli_args("--help")
+def test_help(capsys: CaptureFixture[str]) -> None:
     with pytest.raises(SystemExit) as exception:
         instantiate_from_cli_args(Options)
     assert exception.value.code == 0

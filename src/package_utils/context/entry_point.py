c65bdcdade2b2c14876e1d574cd6c42033e050b4
Quinten <quinten.roets@gmail.com>
2024-02-25 23:21:05 -0800

    add context callback

--- src/package_utils/context/entry_point.py
+++ src/package_utils/context/entry_point.py
@@ -9,13 +9,18 @@ T = TypeVar("T")
 
 
 def create_entry_point(
-    method: Callable[[], T], context: Context[Options, Config, Secrets]
+    method: Callable[[], T],
+    context: Context[Options, Config, Secrets],
+    context_creation_callback: Callable[[Context[Options, Config, Secrets]], None]
+    | None = None,
 ) -> Callable[[], T]:
     def entry_point() -> T:
         if context.loaders.options.model is not None:
             context.options = instantiate_from_cli_args(
                 context.loaders.options.model, documented_object=method
             )
+            if context_creation_callback is not None:
+                context_creation_callback(context)
         return method()
 
     return entry_point

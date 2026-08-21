da45dd1620733bc222672385ec326f999345fc81
Quinten <quinten.roets@gmail.com>
2026-08-20 22:47:32 -0700

    simplify

--- src/package_utils/cli/convertors/parameter.py
+++ src/package_utils/cli/convertors/parameter.py
@@ -15,8 +15,8 @@ class CliParameter:
     annotation: object
 
     def convert(self) -> inspect.Parameter:
-        OptionInfo = typer.Argument if self.is_argument else typer.Option  # noqa: N806
-        info = OptionInfo(path_type=self.extract_path_class())  # type: ignore[arg-type]
+        Info = typer.Argument if self.is_argument else typer.Option  # noqa: N806
+        info = Info(path_type=self.extract_path_class())  # type: ignore[arg-type]
         return self.parameter.replace(annotation=Annotated[self.annotation, info])
 
     def extract_path_class(self) -> type[Path] | None:

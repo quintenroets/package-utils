6bbc0c4f4c411a8229b12f0cd9f3e1fa1aab3236
Quinten <quinten.roets@gmail.com>
2026-08-22 09:28:09 -0700

    deduplicate test annotations

--- tests/cli/models/class_model.py
+++ tests/cli/models/class_model.py
@@ -1,10 +1,7 @@
-from typing import Annotated
-
-import typer
 from superpathlib import Path
 
 from tests.cli.models import dataclass_model
-from tests.cli.models.dataclass_model import Action
+from tests.cli.models.dataclass_model import Action, Parameters
 
 
 class Options:
@@ -14,13 +11,13 @@ class Options:
 
     def __init__(  # noqa: PLR0913, PLR0917
         self,
-        action: Annotated[Action, typer.Argument()] = dataclass_model.Options.action,
-        ignore_paths: Annotated[list[Path] | None, typer.Argument()] = None,
+        action: Parameters.action = dataclass_model.Options.action,
+        ignore_paths: Parameters.optional_ignore_paths = None,
         action_on_error: Action = dataclass_model.Options.action_on_error,
         debug: bool = dataclass_model.Options.debug,  # noqa: FBT001
         config_path: Path = dataclass_model.Options.config_path,
         log_path: Path | None = dataclass_model.Options.log_path,
-        message: str = dataclass_model.Options.message,
+        message: Parameters.message = dataclass_model.Options.message,
         messages: list[str] | None = None,
         optional_message: str | None = dataclass_model.Options.optional_message,
         n_retries: int = 0,

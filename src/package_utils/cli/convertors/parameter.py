0762823f12be4dbdccec7d4974846dfb31f467b8
Quinten Roets <62651391+quintenroets@users.noreply.github.com>
2026-08-22 17:37:12 -0700

    use signature parameters (#84)

--- src/package_utils/cli/convertors/parameter.py
+++ src/package_utils/cli/convertors/parameter.py
@@ -1,9 +1,8 @@
 import collections
 import copy
-import inspect
 import typing
 from collections.abc import Iterator
-from dataclasses import dataclass
+from inspect import Parameter
 from pathlib import Path
 from typing import Annotated, Any
 
@@ -11,36 +10,37 @@ import typer
 from typer.models import ParameterInfo
 
 
-@dataclass
-class CliParameter:
-    parameter: inspect.Parameter
-    annotation: Any
+def convert(parameter: Parameter) -> Parameter:
+    type_ = resolve_type(parameter)
+    metadata = extract_metadata(parameter)
+    infos = (info for info in metadata if isinstance(info, ParameterInfo))
+    info = copy.copy(next(infos, typer.Option()))
+    path_classes = (
+        sub_type for sub_type in extract_types(type_) if is_path_class(sub_type)
+    )
+    info.path_type = typing.cast("type[str] | None", next(path_classes, None))
+    return parameter.replace(annotation=Annotated[type_, info])
 
-    def convert(self) -> inspect.Parameter:
-        infos = (info for info in self.metadata if isinstance(info, ParameterInfo))
-        info = copy.copy(next(infos, typer.Option()))
-        path_classes = (type_ for type_ in self.extract_types() if is_path_class(type_))
-        info.path_type = typing.cast("type[str] | None", next(path_classes, None))
-        return self.parameter.replace(annotation=Annotated[self.type_, info])
 
-    def extract_types(self) -> Iterator[object]:
-        types = collections.deque([self.type_])
-        while types:
-            type_ = types.popleft()
-            sub_types = typing.get_args(type_)
-            if sub_types:
-                types.extend(sub_types)
-            else:
-                yield type_
+def resolve_type(parameter: Parameter) -> Any:
+    annotation = parameter.annotation
+    type_ = annotation.__origin__ if extract_metadata(parameter) else annotation
+    return type_ | None if parameter.default is None else type_
 
-    @property
-    def type_(self) -> Any:
-        type_ = self.annotation.__origin__ if self.metadata else self.annotation
-        return type_ | None if self.parameter.default is None else type_
 
-    @property
-    def metadata(self) -> tuple[Any, ...]:
-        return getattr(self.annotation, "__metadata__", ())
+def extract_metadata(parameter: Parameter) -> tuple[Any, ...]:
+    return getattr(parameter.annotation, "__metadata__", ())
+
+
+def extract_types(root: Any) -> Iterator[object]:
+    types = collections.deque([root])
+    while types:
+        type_ = types.popleft()
+        sub_types = typing.get_args(type_)
+        if sub_types:
+            types.extend(sub_types)
+        else:
+            yield type_
 
 
 def is_path_class(type_: object) -> bool:

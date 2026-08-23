2ca5445ef5e6d792bb70b3fca0cd09fb4aea3409
Quinten <quinten.roets@gmail.com>
2026-08-22 17:34:26 -0700

    use signature parameters

--- src/package_utils/cli/convertors/dataclass.py
+++ src/package_utils/cli/convertors/dataclass.py
@@ -1,19 +1,23 @@
 import dataclasses
 import functools
-import inspect
 import typing
 from collections.abc import Callable, Iterator
-from dataclasses import MISSING, Field, dataclass, field, fields
+from dataclasses import dataclass, field
+from inspect import Parameter
 from typing import TYPE_CHECKING, Any, TypeVar
 
 from . import method
-from .parameter import CliParameter
+from .parameter import extract_types, resolve_type
 
 if TYPE_CHECKING:
     from _typeshed import DataclassInstance  # pragma: nocover
 
 
 T = TypeVar("T")
+MISSING_DEFAULTS = (
+    Parameter.empty,
+    dataclasses._HAS_DEFAULT_FACTORY,  # type: ignore[attr-defined] # noqa: SLF001
+)
 
 
 @dataclass
@@ -23,7 +27,7 @@ class Convertor(method.Convertor[T]):
     argument_prefixes: dict[str, str] = field(default_factory=dict)
 
     def create_cli_entry_method(self) -> Callable[..., T]:
-        @functools.wraps(self.annotated_method)
+        @functools.wraps(self.object, assigned=("__doc__",), updated=())
         def wrapped_method(**kwargs: Any) -> Any:
             specified_kwargs = {k: v for k, v in kwargs.items() if v is not None}
             if self.argument_prefixes:
@@ -36,7 +40,6 @@ class Convertor(method.Convertor[T]):
                 result = self.object(**specified_kwargs)
             return result
 
-        wrapped_method.__doc__ = self.object.__doc__
         return typing.cast("Callable[..., T]", wrapped_method)
 
     def unflatten(self, items: dict[str, Any]) -> None:
@@ -46,47 +49,34 @@ class Convertor(method.Convertor[T]):
                 nested_name = name.removeprefix(prefix + "_")
                 items.setdefault(prefix, {})[nested_name] = items.pop(name)
 
-    def extract_parameters_info(self) -> Iterator[CliParameter]:
-        for field_ in fields(self.object):  # type: ignore[arg-type]
-            if field_.init:
-                yield from self.extract_field_parameters(field_)
-
-    def extract_field_parameters(self, field_: Field[Any]) -> Iterator[CliParameter]:
-        parameter = self.create_cli_parameter(field_)
-        dataclass_ = self.extract_dataclass(parameter)
-        if dataclass_ is None:
-            yield parameter
-        else:
-            yield from self.generate_recursive_parameters(parameter, dataclass_)
+    def extract_parameters(self) -> Iterator[Parameter]:
+        for parameter in super().extract_parameters():
+            cli_parameter = self.create_cli_parameter(parameter)
+            dataclass_ = extract_dataclass(resolve_type(cli_parameter))
+            if dataclass_ is None:
+                yield cli_parameter
+            else:
+                yield from self.generate_recursive_parameters(cli_parameter, dataclass_)
 
     def generate_recursive_parameters(
         self,
-        parameter: CliParameter,
+        parameter: Parameter,
         dataclass_: "type[DataclassInstance]",
-    ) -> Iterator[CliParameter]:
-        name = parameter.parameter.name
-        convertor = Convertor(dataclass_, name_prefix=f"{name}_")
-        for nested_parameter in convertor.extract_parameters_info():
-            self.argument_prefixes[nested_parameter.parameter.name] = name
+    ) -> Iterator[Parameter]:
+        convertor = Convertor(dataclass_, name_prefix=f"{parameter.name}_")
+        for nested_parameter in convertor.extract_parameters():
+            self.argument_prefixes[nested_parameter.name] = parameter.name
             yield nested_parameter
 
-    def create_cli_parameter(self, field_: Field[Any]) -> CliParameter:
-        annotation = self.annotations[field_.name]
-        parameter = inspect.Parameter(
-            self.name_prefix + field_.name,
-            inspect.Parameter.POSITIONAL_OR_KEYWORD,
-            default=None if field_.default is MISSING else field_.default,
+    def create_cli_parameter(self, parameter: Parameter) -> Parameter:
+        default = None if parameter.default in MISSING_DEFAULTS else parameter.default
+        return parameter.replace(
+            name=self.name_prefix + parameter.name,
+            kind=Parameter.POSITIONAL_OR_KEYWORD,
+            default=default,
         )
-        return CliParameter(parameter, annotation)
 
-    @classmethod
-    def extract_dataclass(
-        cls,
-        parameter: CliParameter,
-    ) -> "type[DataclassInstance]|None":
-        types = (
-            type_
-            for type_ in parameter.extract_types()
-            if dataclasses.is_dataclass(type_)
-        )
-        return typing.cast("type[DataclassInstance]|None", next(types, None))
+
+def extract_dataclass(root: Any) -> "type[DataclassInstance]|None":
+    types = (type_ for type_ in extract_types(root) if dataclasses.is_dataclass(type_))
+    return typing.cast("type[DataclassInstance]|None", next(types, None))
--- src/package_utils/cli/convertors/method.py
+++ src/package_utils/cli/convertors/method.py
@@ -1,10 +1,9 @@
-import inspect
-import typing
 from collections.abc import Callable, Iterator
 from dataclasses import dataclass
+from inspect import Parameter, Signature, signature
 from typing import Generic, TypeVar
 
-from .parameter import CliParameter
+from .parameter import convert
 
 T = TypeVar("T")
 Method = Callable[..., T]
@@ -14,27 +13,14 @@ Method = Callable[..., T]
 class Convertor(Generic[T]):
     object: Method[T]
 
-    @property
-    def annotated_method(self) -> Method[T]:
-        is_class = inspect.isclass(self.object)
-        return self.object.__init__ if is_class else self.object  # type: ignore[return-value]
-
-    def __post_init__(self) -> None:
-        method = self.annotated_method
-        self.annotations = typing.get_type_hints(method, include_extras=True)
-
     def run(self) -> Method[T]:
         method = self.create_cli_entry_method()
-        parameters = [
-            parameter.convert() for parameter in self.extract_parameters_info()
-        ]
-        method.__signature__ = inspect.Signature(parameters=parameters)  # type: ignore[attr-defined]
+        parameters = [convert(parameter) for parameter in self.extract_parameters()]
+        method.__signature__ = Signature(parameters=parameters)  # type: ignore[attr-defined]
         return method
 
     def create_cli_entry_method(self) -> Method[T]:
         return self.object
 
-    def extract_parameters_info(self) -> Iterator[CliParameter]:
-        for parameter in inspect.signature(self.object).parameters.values():
-            annotation = self.annotations[parameter.name]
-            yield CliParameter(parameter, annotation)
+    def extract_parameters(self) -> Iterator[Parameter]:
+        yield from signature(self.object, eval_str=True).parameters.values()

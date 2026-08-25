4458ea17add5bfdf7b552cbc949b4c067734af30
Quinten <quinten.roets@gmail.com>
2026-08-24 21:14:03 -0700

    support string annotated secrets

---
+++ tests/context/models/models_with_string_annotations.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+from superpathlib import Path
+
+
+@dataclass
+class Options:
+    debug: bool = False
+    config_path: Path = Path.draft
+
+
+@dataclass
+class Config:
+    output_path: Path | None = None
+    secrets_path: Path | None = None
+
+
+@dataclass
+class ApiSecrets:
+    id: str
+    token: str
+
+
+@dataclass
+class Secrets:
+    token: str
+    api: ApiSecrets
